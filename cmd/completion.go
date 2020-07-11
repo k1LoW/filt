d8919a2e437467bf366d71ed1523f807b02ee829
k1LoW <k1lowxb@gmail.com>
2020-07-11 14:37:59 +0900

    bonsai

--- cmd/completion.go
+++ cmd/completion.go
@@ -70,12 +70,12 @@ filt completion zsh > $fpath[1]/_filt
 
 		switch sh {
 		case "bash":
-			if err := rootCmd.GenBashCompletion(o); err != nil {
+			if err := cmd.Root().GenBashCompletion(o); err != nil {
 				_ = o.Close()
 				printFatalln(cmd, err)
 			}
 		case "zsh":
-			if err := rootCmd.GenZshCompletion(o); err != nil {
+			if err := cmd.Root().GenZshCompletion(o); err != nil {
 				_ = o.Close()
 				printFatalln(cmd, err)
 			}
